e34f439759f3b8830ba7197fc16a214c2dc19192
Youssef AbuTaleb <youssefabdelmoneim134@gmail.com>
2024-01-07 15:53:23 +0200

    handle cities context with useReducer

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,8 +1,9 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import BackButton from "./BackButton";
+import Spinner from "./Spinner";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -14,7 +15,7 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const { getCity, currentCity } = useCities();
+  const { getCity, currentCity, isLoading } = useCities();
   useEffect(
     function () {
       getCity(id);
@@ -23,7 +24,7 @@ function City() {
   );
 
   const { cityName, emoji, date, notes } = currentCity;
-
+  if (isLoading) return <Spinner />;
   return (
     <div className={styles.city}>
       <div className={styles.row}>
